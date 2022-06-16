44aeb39b18159899d37ef68f71fb77c5151a6a47
Jake Sinclair <100993873+JakeSincl@users.noreply.github.com>
2022-06-16 17:34:37 +0000

    Added a welcome page with buttons to reach the appropriate login pages, a menu bar to access the support page and a usage guide for login in.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+#, request, session
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello from Flask!'
+def welcome():
+    return render_template("welcome.html")
 
-app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0')
